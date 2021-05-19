cdb0cbed49bd843047a84c694db2526eabf8dd93
wangwc <wwc953@163.com>
2021-05-19 08:55:19 +0800

    去除无用方法

--- provider2/src/main/java/com/wwc/provider2/dao/MsgInfoMapper.java
+++ provider2/src/main/java/com/wwc/provider2/dao/MsgInfoMapper.java
@@ -7,17 +7,12 @@ import java.util.List;
 
 @Mapper
 public interface MsgInfoMapper {
-    int deleteByPrimaryKey(String id);
-
-    int insert(MsgInfo record);
 
     int insertSelective(MsgInfo record);
 
     MsgInfo selectByPrimaryKey(String id);
 
     int updateByPrimaryKeySelective(MsgInfo record);
-
-    int updateByPrimaryKey(MsgInfo record);
 
     List<MsgInfo> queryList(MsgInfo msgInfo);
 }
